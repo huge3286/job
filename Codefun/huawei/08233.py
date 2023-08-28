b9e8db8100a8d58d1928bc19a5b783ffbd00726e
huge3286 <prestige3286@gmail.com>
2023-08-28 17:32:10 +0800

    first commit

--- Codefun/huawei/08233.py
+++ Codefun/huawei/08233.py
@@ -6,37 +6,42 @@ g = []
 for i in range(m):
     g.append(list(map(int, input().split())))
 
-dir = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+dir = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-from collections import deque
-
-ans = [[0] * n for _ in range(m)]
-
-for i in range(m):
-    for j in range(n):
-        if g[i][j] != 0:
-            ans[i][j] = float("inf")
+res = [[float("inf")] * n for _ in range(m)]
 
 # bfs
-res = 0
+from collections import deque
+
 q = deque([[x, y, 0]])
 while q:
     x, y, t = q.popleft()
+
     if -1 < x < m and -1 < y < n and g[x][y] != 0:
-        if t > ans[x][y]:
+        if t >= res[x][y]:
             continue
-        ans[x][y] = t
+
+        res[x][y] = t
+        nt = t + g[x][y]
 
         for dx, dy in dir:
-            q.append([x + dx, y + dy, t + g[x][y]])
+            nx, ny = x + dx, y + dy
+            if -1 < nx < m and -1 < ny < n and g[x][y] != 0:
+                if nt >= res[nx][ny]:
+                    continue
+                q.append([nx, ny, nt])
 
-res = 0
-for line in ans:
-    for item in line:
-        if item < float("inf"):
-            res = max(res, item)
-        else:
-            res = -1
+ans = 0
+out_flag = 0
+for i in range(m):
+    for j in range(n):
+        if res[i][j] < float("inf"):
+            ans = max(ans, res[i][j])
+        elif g[i][j] > 0:
+            ans = -1
+            out_flag = 1
             break
+    if out_flag:
+        break
 
-print(res)
+print(ans)
